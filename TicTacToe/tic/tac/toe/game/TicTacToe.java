f9c0f0bf65cd75fe383bfa350199270ef492d3af
DevMind <DevMind@192.168.1.14>
2023-10-15 17:59:43 +0200

    update board

--- TicTacToe/tic/tac/toe/game/TicTacToe.java
+++ TicTacToe/tic/tac/toe/game/TicTacToe.java
@@ -6,13 +6,11 @@ public class TicTacToe {
 	public static void main(String[] args) throws InterruptedException {
 		
 		char[][] board = { 
-				
 				{ ' ', '|', ' ', '|', ' ' },
 				{ '-', '+', '-', '+', '-' }, 
 				{ ' ', '|', ' ', '|', ' ' },
 				{ '-', '+', '-', '+', '-' }, 
-				{ ' ', '|', ' ', '|', ' ' } 
-		};
+				{ ' ', '|', ' ', '|', ' ' } };
 
 		GameBoard gameBoard = new GameBoard();
 		ChoosePosition choosePosition = new ChoosePosition();		
